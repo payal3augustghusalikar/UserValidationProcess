b31d123549470a3bd2de5ec1e1aa416b1080c894
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-11-27 23:17:06 +0530

    Added : validate last name

--- src/main/java/com/uservalidationprocess/UserValidationProcess.java
+++ src/main/java/com/uservalidationprocess/UserValidationProcess.java
@@ -4,12 +4,14 @@ import java.util.regex.Pattern;
 
 public class UserValidationProcess {
     String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
+    String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
 
     public boolean validateFirstName(String firstName)
     {
         return Pattern.matches(firstNamePattern,firstName);
     }
 
-    public void validateFirstName() {
+    public boolean validateLastName(String lastName) {
+        return Pattern.matches(lastNamePattern,lastName);
     }
 }
