17d3c47aad840e04c57740819c104817e7240725
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-11-28 01:56:19 +0530

    Refactored : checked and validate email samples

--- src/main/java/com/uservalidationprocess/UserValidationProcess.java
+++ src/main/java/com/uservalidationprocess/UserValidationProcess.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class UserValidationProcess {
     String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
     String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
-    String emailPattern = "^([a-z]{3,})([.]{0,1}[a-z]*)@([a-z]{2}).([a-z]{2})([.]{1}[a-z]{2}){0,1}$";
+    String emailPattern = "^([a-z]{3,})([\'.\'-\'+]{0,1}[a-z0-9]*)@([a-z]{1,4}).([a-z]{2,3})(^[.]{0,1}[a-z]{2,3}){0,1}$";
     String mobileNumberPattern = "^([0-9]{2})[ ]([1-9]{1}[0-9]{9})$";
     String passwordPattern = "^(.*[A-Z]{1,}.*[0-9]{1,}.*[~!@#$%^&*]?[A-Za-z0-9]{5,})$";
 
