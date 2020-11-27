2e583ef1bf7da83e66c79b8f6cc31ca959ef17cc
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-11-27 20:29:47 +0530

    Added : Welcome message

---
+++ src/main/java/com/uservalidationprocess/UserValidation.java
@@ -0,0 +1,7 @@
+package com.uservalidationprocess;
+
+public class UserValidation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User validation process");
+    }
+}
